4175ac3469278d4744b80405428c82c89cb7e80e
Jonny <thejinbox@gmail.com>
2013-01-20 12:12:20 +0000

    final

--- HPC_Sequential/Sequential.cpp
+++ HPC_Sequential/Sequential.cpp
@@ -2,9 +2,14 @@
 #include<cmath>
 #include<fstream>
 using namespace std;
-#define N 4
+#define N 32
 #define MAXITER 1000000
 #define TOL 0.000001
+
+void printPlotData(double phi[N+1][N+1],double sol[N+1][N+1],double x[N+1],double y[N+1], int iteration, double error, ofstream& plotData);
+double getAnalyticalSolution(double x, double y, int precision, const double PI);
+double getFourierCoeficient(int n, const double PI);
+
 int main(){
 	//Initializing data structures
 	double const PI=3.14159265;
@@ -27,9 +32,14 @@ int main(){
 
 	//File outputs
 	ofstream errorLog("error_log.txt",ios_base::trunc);
-	ofstream plot("plot_data", ios_base::trunc);
+	ofstream plotData("plot_data.txt", ios_base::trunc);
 
-	//
+	//Analytical Solution
+	for(int i=0; i<=N; i++)
+		for(int j=0; j<=N; j++)
+			sol[i][j]=getAnalyticalSolution(x[i],y[j],100,PI);
+
+	//Jacobi
 	int k=0;
 	bool iterate=true;
 	while(iterate && k<MAXITER){
@@ -42,11 +52,43 @@ int main(){
 		for(int i=1;i<N;i++)
 			for(int j=1;j<N;j++)
 				old[i][j]=phi[i][j];
-		if(iteration_error<=TOL)
+		errorLog<<"Iteration "<<k<<": "<<iteration_error<<endl;
+
+		if(iteration_error<=TOL){
+			printPlotData(phi,sol,x,y,k,iteration_error,plotData);
 			iterate=false;
+		}
 		k++;
 	}
-	cout<<x[0]<<","<<y[1]<<" = "<<phi[0][1]<<endl;
-	system("pause");
+	
+	//system("pause");
 	return 0;
+}
+
+double getAnalyticalSolution(double x, double y, int precision, const double PI){
+	//first summation element
+	double alpha=getFourierCoeficient(1,PI);
+	double u=alpha*sin(PI*y)*(cosh(PI*x)-sinh(PI*x));
+	if(precision>2){	//element 2 introduces a NaN (division by zero)
+		for(int n=3; n<=precision; n++){
+			alpha=getFourierCoeficient(n,PI);
+			u+=alpha*sin(n*PI*y)*(cosh(n*PI*x)-sinh(n*PI*x));
+		}
+	}
+	return u;
+}
+
+double getFourierCoeficient(int n, const double PI){
+	double num=4*(-1+cos(n*PI));
+	double den=(-4*n+pow(n,3))*PI;
+	return num/den;
+}
+
+void printPlotData(double phi[N+1][N+1],double sol[N+1][N+1],double x[N+1],double y[N+1], int iteration, double error, ofstream& out){
+	out<<"N = "<<N<<"\nRequired iterations: "<<iteration<<endl;
+	for(int i=0; i<=N; i++)
+		for(int j=0; j<=N; j++)
+			out<<"("<<x[i]<<","<<y[j]<<") - "<<"Estimated solution: "<<phi[i][j]<<" Analytic solution: "<<sol[i][j]<<endl;
+	
+
 }
